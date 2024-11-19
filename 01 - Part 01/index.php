f4bc472522bbf5b4ffaafc6c64e3f22488747578
Rahatul99 <rahatul99@gmail.com>
2024-11-19 19:44:54 +0600

    Fetching data with fsockopen

--- 01 - Part 01/index.php
+++ 01 - Part 01/index.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Weather\FakeWeatherFetcher;
+use App\Weather\RemoteWeatherFetcher;
 
 require __DIR__ . '/inc/all.inc.php';
 
-$fetcher = new FakeWeatherFetcher();
-$info = $fetcher->fetch('New York City');
+$fetcher = new RemoteWeatherFetcher();
+$info = $fetcher->fetch('Dhaka');
 // var_dump($info);
 
 require __DIR__ . '/views/index.view.php';
